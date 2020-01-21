0b61fdeb9e1970449c56598c1d8af26025cae1ab
Mansoor <hussainmansoor876@gmail.com>
2020-01-22 00:43:51 +0500

    done

---
+++ examples/app.js
@@ -0,0 +1,54 @@
+axios.get('https://star-rating123.herokuapp.com/get/all-company')
+    .then((result) => {
+        console.log('result', result)
+    })
+
+axios.get('http://localhost:5001/get/peding-company')
+    .then((result) => {
+        const { data } = result
+        console.log(data)
+        data['data'].map((v, i) => {
+            return document.getElementById("companyBody").innerHTML += `<tr id=${v._id}>
+            <th scope="row">
+              <div class="media align-items-center">
+                <a href="#" class="avatar rounded-circle mr-3">
+                  <img alt="Image placeholder" src="../assets/img/theme/no-user-image.png">
+                </a>
+                <div class="media-body">
+                  <span class="mb-0 text-sm">${v.name}</span>
+                </div>
+              </div>
+            </th>
+            <td>
+              ${v.contactEmail}
+            </td>
+            <td>
+              <span class="badge badge-dot mr-4">
+                <i class="bg-warning"></i> Monthly Pkg
+              </span>
+            </td>
+            <td>
+              <div class="avatar-group">
+                <span class="badge badge-dot mr-4">${v.contactNo}</span>
+              </div>
+            </td>
+            <td>
+              <div class="d-flex align-items-center">
+                <span class="badge badge-dot mr-4">${v.url}</span>
+              </div>
+            </td>
+            <td class="text-right">
+              <div class="dropdown">
+                <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                  <i class="fas fa-ellipsis-v"></i>
+                </a>
+                <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+                  <a class="dropdown-item" href="#">Action</a>
+                  <a class="dropdown-item" href="#">Another action</a>
+                  <a class="dropdown-item" href="#">Something else here</a>
+                </div>
+              </div>
+            </td>
+            </tr>`
+        })
+    })
