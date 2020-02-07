cbe53ed78948d63ef4b65bd900805736f8fdb93a
Mansoor <hussainmansoor876@gmail.com>
2020-02-08 00:32:45 +0500

    done

--- js/review.js
+++ js/review.js
@@ -1,4 +1,40 @@
 axios.get('http://localhost:5001/get/peding-reviews')
     .then((response) => {
         console.log(response)
+        document.getElementById('reviewList').innerHTML = `<tr>
+        <th scope="row">
+          <div class="media align-items-center">
+            <div class="media-body">
+              <span class="mb-0 text-sm">User Name</span>
+            </div>
+          </div>
+        </th>
+        <td>
+          <div class="align-items-center">
+            <span class="mr-2 white-spce" style="white-space: normal;">Lorem Ipsum is simply dummy text of
+              the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
+              ever since the 1500s</span>
+          </div>
+        </td>
+        <td>
+          <video width="320" height="240" controls>
+            <source src="movie.mp4" type="video/mp4">
+            <source src="movie.ogg" type="video/ogg">
+            Your browser does not support the video tag.
+          </video>
+        </td>
+        <td class="text-right">
+          <div class="dropdown">
+            <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <i class="fas fa-ellipsis-v"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right dropdown-menu-arrow">
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </td>
+      </tr>`
     })
