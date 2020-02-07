34b9361eb6cf6f25cc0d4d331f501a55a091c5ec
Mansoor <hussainmansoor876@gmail.com>
2020-02-08 00:37:29 +0500

    done

--- js/review.js
+++ js/review.js
@@ -1,20 +1,19 @@
 axios.get('http://localhost:5001/get/peding-reviews')
     .then((response) => {
         const { data } = response
+        console.log(data)
         data['data'].map((v, i) => {
             return document.getElementById('reviewList').innerHTML += `<tr>
             <th scope="row">
               <div class="media align-items-center">
                 <div class="media-body">
-                  <span class="mb-0 text-sm">User Name</span>
+                  <span class="mb-0 text-sm">${v.reveiwerName}</span>
                 </div>
               </div>
             </th>
             <td>
               <div class="align-items-center">
-                <span class="mr-2 white-spce" style="white-space: normal;">Lorem Ipsum is simply dummy text of
-                  the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text
-                  ever since the 1500s</span>
+                <span class="mr-2 white-spce" style="white-space: normal;">${v.feedback}</span>
               </div>
             </td>
             <td>
